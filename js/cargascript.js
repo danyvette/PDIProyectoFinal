932e9b246c7ee831fa9276a704456b2e8538ed7b
danyvette <al02894612@tecmilenio.mx>
2023-10-29 00:08:34 -0600

    Mensaje

---
+++ js/cargascript.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () =>{
+    const contenedor_carga= document.querySelector(".contenedor_carga")
+    contenedor_carga.style.opacity = 0
+    contenedor_carga.style.visibility = 'hidden'
+})
